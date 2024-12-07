7d5452b010ef7db0d618a79ef677e74dedd7ab2f
Abdullah khan <akhan283785@gmail.com>
2024-12-07 10:07:34 +0530

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+  const [vlv, setVlv] = useState("");
+  const [itemSearch, setItemSearch] = useState(fruits);
+
+  useEffect(() => {
+    if (vlv) {
+      setItemSearch(itemSearch.filter((item) => item.includes(vlv)));
+    } else {
+      setItemSearch(fruits);
+    }
+  }, [vlv]);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input type="text" value={vlv} onChange={(e) => setVlv(e.target.value)} />
+      <ul>
+        {itemSearch.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
